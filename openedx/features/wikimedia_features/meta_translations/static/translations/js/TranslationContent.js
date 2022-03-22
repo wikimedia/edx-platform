527a6cefa09fa5af1b43db4c8424943f3c12d0c1
AliSalman <alisalman@192.168.10.8>
2022-03-22 13:45:53 +0500

    fix bugs

--- openedx/features/wikimedia_features/meta_translations/static/translations/js/TranslationContent.js
+++ openedx/features/wikimedia_features/meta_translations/static/translations/js/TranslationContent.js
@@ -1,32 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const TranslationContent = ({context}) => {
-    const [sections, setSections] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-
-    useEffect(() => {
-        setIsLoading(true);
-        setProducts([]);
-        const fetchData = async () => {
-          const jsonData = await fetchSections(catalogId, page);
-          setProducts(jsonData.results);
-          setCount(jsonData.count);
-          setIsLoading(false);
-        };
-        fetchData();
-      }, [page, catalogId, triggerDelete]);
-  
+export default function TranslationContent({ context }) {
     return (
-      <>
-        {isUserFetching && <CircularProgress />}
-        {!isUserFetching && (
-          <Layout>
-            <Routes />
-            <Toaster position='top-right' reverseOrder={false} />
-          </Layout>
-        )}
-      </>
-    );
-  };
-  
-  export default App;
+        <div>Helllo World</div>
+    )
+}
