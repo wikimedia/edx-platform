00a0f89936309366bd1a5800fcbbe807770c483b
AliSalman <alisalman@192.168.10.3>
2022-03-24 11:31:02 +0500

    add apis

--- openedx/features/wikimedia_features/meta_translations/views.py
+++ openedx/features/wikimedia_features/meta_translations/views.py
@@ -9,11 +9,8 @@ from openedx.features.wikimedia_features.meta_translations.utils import get_base
 
 
 @login_required
-def render_translation_home(request, course_id):
-    base_course_id = get_base_course_id(course_id)
+def render_translation_home(request):
     return render_to_response('translations.html', {
-        'course_id': course_id,
-        'base_course_id': base_course_id,
         'uses_bootstrap': True,
         'login_user_username': request.user.username,
     })
