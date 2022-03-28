433cdfb4c463a18bbb2477d4c583aecbecf32e79
AliSalman <alisalman@192.168.10.2>
2022-03-28 19:20:30 +0500

    update version api

--- openedx/features/wikimedia_features/meta_translations/api/v0/views.py
+++ openedx/features/wikimedia_features/meta_translations/api/v0/views.py
@@ -6,6 +6,7 @@ from lms.djangoapps.courseware.courses import get_course_by_id
 from opaque_keys.edx.keys import CourseKey
 from django.utils.translation import ugettext as _
 from openedx.features.wikimedia_features.meta_translations.api.v0.utils import get_courses_of_base_course, get_outline_course_to_sections
+from openedx.features.wikimedia_features.meta_translations.models import CourseTranslation
 from rest_framework import generics
 from rest_framework import status
 from rest_framework.response import Response
@@ -62,15 +63,24 @@ class GetSubSectionContent(generics.RetrieveAPIView):
         return Response(json.dumps(data), status=status.HTTP_200_OK)
 
 class GetCoursesVersionInfo(generics.RetrieveAPIView):
-    def _course_version_format(self, courses_id):
-        return {
-            'course_id': str(courses_id),
-            'base_course_ids': get_courses_of_base_course(courses_id)
+    def _course_version_format(self, course_key):
+        course = get_course_by_id(course_key)
+        base_course_obj = {
+            'course_id': str(course.id),
+            'course_title': str(course.display_name),
+            'language': course.language,
+            'rerun': get_courses_of_base_course(course.id)
         }
-    
+        return str(course.id), base_course_obj
+
+
     def get(self, request, *args, **kwargs):
         user_courses, _ = get_courses_accessible_to_user(request)
-        courses_ids = [course.id for course in user_courses]
-        data = [self._course_version_format(course_id) for course_id in courses_ids]
-        return Response(json.dumps(data), status=status.HTTP_200_OK)
+        course_keys = [course.id for course in user_courses]
+        translated_courses = CourseTranslation.objects.filter(base_course_id__in=course_keys)
+        base_course_keys = [translated_course.base_course_id for translated_course in translated_courses]
+        base_course_keys = list(set(base_course_keys))
+        data = [self._course_version_format(key) for key in base_course_keys]
+        json_data = dict(data)
+        return Response(json.dumps(json_data), status=status.HTTP_200_OK)
         
