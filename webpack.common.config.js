58a2bdd6935231b8afc10c9fb31c6b5ef806335f
AliSalman <alisalman@192.168.10.4>
2022-04-01 17:59:31 +0500

    add translation app with apis

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -118,6 +118,7 @@ module.exports = Merge.smart({
             // Wikimedia
             Messenger: './openedx/features/wikimedia_features/messenger/static/messenger/js/Messenger.js',
             Translations: './openedx/features/wikimedia_features/meta_translations/static/translations/js/Translations.js',
+            
             AnnouncementsView: './openedx/features/announcements/static/announcements/jsx/Announcements.jsx',
             CookiePolicyBanner: './common/static/js/src/CookiePolicyBanner.jsx',
 
