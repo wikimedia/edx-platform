250a1bc319db9b753c9fa6703e80f3f8b34d9cf2
arbirali <rahat.ali@arbisoft.com>
2022-04-04 20:56:59 +0500

    Feedback --> Styled moved to theme directory

--- openedx/features/wikimedia_features/meta_translations/static/translations/js/Translations.js
+++ openedx/features/wikimedia_features/meta_translations/static/translations/js/Translations.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import TranslationContent from "./TranslationContent";
 
-import '../../scss/main.scss';
-
 export class Translations {
   constructor(context) {
     ReactDOM.render(<TranslationContent context={context} />, document.getElementById("root"));
