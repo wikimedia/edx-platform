b2acfdd9642021d09302d0ea071c9c89c40005e4
AliSalman <alisalman@192.168.10.5>
2022-04-13 14:17:27 +0500

    add translation app and apis

--- openedx/features/wikimedia_features/meta_translations/apps.py
+++ openedx/features/wikimedia_features/meta_translations/apps.py
@@ -5,15 +5,19 @@ from django.apps import AppConfig
 from edx_django_utils.plugins import PluginURLs, PluginSettings
 from openedx.core.djangoapps.plugins.constants import ProjectType, SettingsType
 
-
 class MetaTranslationsConfig(AppConfig):
     name = 'openedx.features.wikimedia_features.meta_translations'
     plugin_app = {
         PluginURLs.CONFIG: {
             ProjectType.CMS: {
-                PluginURLs.NAMESPACE: 'meta_translations',
-                PluginURLs.REGEX: '^meta/',
-                PluginURLs.RELATIVE_PATH: 'urls',
+                    PluginURLs.NAMESPACE: 'meta_translations',
+                    PluginURLs.REGEX: '^meta_translations/',
+                    PluginURLs.RELATIVE_PATH: 'urls',
+                },
+        },
+        PluginSettings.CONFIG: {
+            ProjectType.CMS: {
+                SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: 'settings.common'},
             }
         }
     }
