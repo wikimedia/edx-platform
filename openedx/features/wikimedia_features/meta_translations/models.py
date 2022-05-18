77cbe11f0b3a0382ae4a290f77a9f8277ad902c8
AliSalman <alisalman@A006-00525.arbisoft.com>
2022-05-18 12:16:28 +0500

    add parsed_key filed in course data table

--- openedx/features/wikimedia_features/meta_translations/models.py
+++ openedx/features/wikimedia_features/meta_translations/models.py
@@ -164,6 +164,7 @@ class CourseBlockData(models.Model):
     course_block = models.ForeignKey(CourseBlock, on_delete=models.CASCADE)
     data_type = models.CharField(max_length=255)
     data = models.TextField()
+    parsed_keys = models.TextField(default=None)
     content_updated = models.BooleanField(default=False)
     mapping_updated = models.BooleanField(default=False)
 
