7745bcdc5e4307bd999b98f132b9fd1af84784ab
Ali Salman <88362079+Ali-Salman29@users.noreply.github.com>
2022-06-17 12:25:33 +0500

    add version feature (#104)
    Co-authored-by: AliSalman <alisalman@A006-00525.local>

--- openedx/features/wikimedia_features/meta_translations/static/translations/js/components/Actions.js
+++ openedx/features/wikimedia_features/meta_translations/static/translations/js/components/Actions.js
@@ -1,47 +1,128 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Select from 'react-select';
 import useUpdate from '../hooks/useUpdate';
 
 function Actions (props) {
 
-  const { approved, approveAll, enableApproveButton, context } = props;
+  const { approved, versionStatus, enableApproveButton, destinationFlag, context } = props;
+  const { applied, applied_version, versions } = versionStatus
 
-  const { approveCourseOutline, approveRecursiveCourseOutline } = useUpdate(context);
-
-  const title = !enableApproveButton? 'No Translations': approved? 'Approved': 'Approve'
+  const { approveCourseOutline, updateTranslation,
+    updateTranslationToInitialState, applyCourseVersion } = useUpdate(context);
   
-  const showApproveAllButton = false
+  
+  const [approveTrigger, setApproveTrigger] = React.useState(approved)
+  
+  const [buttonsVisibility, setButtonsVisibility] = React.useState({apply: false, approve: true})
+
+  const [selectedOption, setSelectedOption] = React.useState({value:-1, label: 'pending translation'})
+  
+  const [options, setOptions]  = React.useState({})
+
+  const [enableApplyButton, setEnableApplyButton] = React.useState(false)
+
+  const approveTitle = (!destinationFlag? 'Translation is Disabled':
+                        !enableApproveButton? 'Incomplete Translation': 
+                        approved? 'Approved': 'Approve')
+  
+  const applyTitle = !enableApplyButton ? 'Applied' : applied_version==selectedOption.value ? 'Apply Now': 'Apply'
+  
+  const updateOptionsFromVersion = () => {
+    let newOptions = {
+      recent: approved ? []: [{value:-1, label: 'pending translation'}], 
+      applied: [],
+      other: []
+    }
+    versions.forEach((version, index) => {
+      if (applied && version.id == applied_version){
+          newOptions.applied.push({value: version.id, label: version.date})
+      } else {
+        newOptions.other.push({value: version.id, label: version.date})
+      }
+    });
+    let other = [...newOptions.other].reverse()
+    newOptions.other = [...other]
+    setOptions(newOptions)
+  }
+
+  useEffect(() => {
+    updateOptionsFromVersion(approved);
+    if (approveTrigger) {
+      let last_element = versions.slice(-1)[0]
+      setSelectedOption({value: last_element.id, label: last_element.date});
+      setButtonsVisibility({apply: true, approve: false});
+      setApproveTrigger(false);
+      setEnableApplyButton(last_element.id != applied_version || !applied);
+    } else {
+      setEnableApplyButton(selectedOption.value != applied_version || !applied);
+    }
+  }, [versions, applied, applied_version, approved]);
+
+  const handleChange = (option) => {
+    if (option != selectedOption){
+      setSelectedOption(option);
+      setEnableApplyButton(option.value != applied_version || !applied);
+      if (option.value != -1 ){
+        updateTranslation({version_id: option.value, ...props});
+        setButtonsVisibility({apply: true, approve: false});
+      } else {
+        updateTranslationToInitialState({...props});
+        setButtonsVisibility({apply: false, approve: true});
+      }
+    }
+  };
+
+  const handleApply = (e) => {
+    e.stopPropagation();
+    if (enableApplyButton){
+      applyCourseVersion({version_id: selectedOption.value, ...props});
+    }
+  }
 
   const hanldeApprove = (e) => {
     e.stopPropagation();
     if (!approved && enableApproveButton){
-        const options = {
-          approved: true
-        }
-        approveCourseOutline({options, ...props});
+        approveCourseOutline({...props});
+        setApproveTrigger(true);
     }
   }
 
-  const hanldeApproveAll = (e) => {
-    e.stopPropagation();
-    const options = {
-      approved: true,
-      apply_all: true
-    }
-    approveRecursiveCourseOutline({options, ...props});
-  }
   return (
     <div className="btn-box">
       {
-        showApproveAllButton && approveAll && (
-          <span className="btn approveAll" title="Approve All" onClick={hanldeApproveAll}>
-            <i className="fa fa-list" aria-hidden="true"></i>
+        <Select
+          className="options"
+          value={selectedOption}
+          onChange={handleChange}
+          options={[
+            {
+              label: 'recent',
+              options: options.recent,
+            },
+            {
+              label: 'applied',
+              options: options.applied,
+            },
+            {
+              label: 'other',
+              options: options.other,
+            }
+          ]}
+        />
+      }
+      {
+        buttonsVisibility.apply && (
+          <span className={`btn ${!enableApplyButton? 'disabled': ''}`} title={applyTitle} onClick={handleApply}>
+            APPLY
           </span>
         )
       }
       {
-        <span className={`btn ${!enableApproveButton? 'disabled': approved? 'approved': ''}`} title={title} onClick={hanldeApprove}>
-          <i className="fa fa-check" aria-hidden="true"></i>
-        </span>
+        buttonsVisibility.approve && (
+          <span className={`btn ${!enableApproveButton? 'disabled': ''}`} title={approveTitle} onClick={hanldeApprove}>
+            APPROVE
+          </span>
+        )
       }
     </div>
   )
