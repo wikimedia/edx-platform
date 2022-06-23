c43b0aecdae53e870772a55ebc08aabf54e201df
tehreem-sadat <tehreem.sadat@arbisoft.com>
2022-06-23 10:30:39 +0500

    Advance problem types list in not working in yml - remove it from yml and add it to production settings

--- openedx/features/wikimedia_features/wikimedia_general/settings/common.py
+++ openedx/features/wikimedia_features/wikimedia_general/settings/common.py
@@ -1,5 +1,5 @@
 
-"""Settings"""
+"""Common Settings"""
 
 
 def plugin_settings(settings):
@@ -7,3 +7,25 @@ def plugin_settings(settings):
     Required Common settings
     """
     settings.FEATURES['ENABLE_DEFAULT_COURSE_MODE_CREATION'] = False
+    ADVANCED_PROBLEM_TYPES = [
+        {
+            'component': 'drag-and-drop-v2',
+            'boilerplate_name': None
+        },
+        {
+            'component': 'staffgradedxblock',
+            'boilerplate_name': None
+        },
+        {
+            'component': 'poll',
+            'boilerplate_name': None
+        },
+        {
+            'component': 'done',
+            'boilerplate_name': None
+        },
+        {
+            'component': 'lti_consumer',
+            'boilerplate_name': None
+        }
+    ]
