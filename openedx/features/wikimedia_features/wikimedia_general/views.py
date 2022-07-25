dfb1362ea9bc8b96d009e73a9b7c04fd329491eb
tehreem-sadat <tehreem.sadat@arbisoft.com>
2022-07-25 05:11:37 +0500

    Add progress in context

--- openedx/features/wikimedia_features/wikimedia_general/views.py
+++ openedx/features/wikimedia_features/wikimedia_general/views.py
@@ -10,93 +10,115 @@ from lms.djangoapps.grades.api import CourseGradeFactory
 
 class WikimediaProgressFragmentView(EdxFragmentView):
     """
+    View to render Wikimedia course progress tab
     """
     def _get_grade_dict(self, course_grade):
+        """
+        Extract grade dict in following format from edX grade classes for subsections and problem components.
+        {
+            "subsection1_block_id": {
+                "score": 1/2,
+                "graded": True
+            },
+            "problem1_block_id": {
+                "score": 1/1,
+                "graded": True
+            },
+            "problem2_block_id": {
+                "score": 0/1,
+                "graded": True
+            }
+        }
+        """
         data = {}
+        if not course_grade:
+            return data
+
         for chapter in course_grade.chapter_grades.values():
             subsections = chapter.get('sections', [])
             for subsection in subsections:
                 subsection_grade = subsection.graded_total
+                subsection_score = "-"
+                if subsection_grade and subsection_grade.possible:
+                    subsection_score = "{}/{}".format(round(subsection_grade.earned), round(subsection_grade.possible))
                 data.update({str(subsection.location): {
-                    'score': "{}/{}".format(subsection_grade.earned, subsection_grade.possible),
+                    'score': subsection_score,
                     'graded': subsection_grade.graded
                 }})
                 for problem_location, problem_grade in subsection.problem_scores.items():
                     data.update({str(problem_location): {
-                        'score': "{}/{}".format(problem_grade.earned, problem_grade.possible),
+                        'score': "{}/{}".format(round(problem_grade.earned), round(problem_grade.possible)),
                         'graded': problem_grade.graded,
-                        'attempted': True if problem_grade.first_attempted else False
                     }})
         return data
 
-    def _update_context_with_score(self, block, grade_dict):
-        grade_data = grade_dict.get(block.get('id'))
-        if grade_data:
-            block.update(grade_data)
-        for child in block.get('children', []):
-            self._update_context_with_score(child, grade_dict)
+    def _update_context_with_score_and_progress(self, block, grade_dict):
+        """
+        Update given block dict with grade and progress percentage info.
+        """
+        if not block:
+            return
+
+        block_grade_data = grade_dict.get(block.get('id'))
+        if block_grade_data:
+            block.update(block_grade_data)
+
+        progress = 0
+        unit_count = 0
+        children = block.get('children', [])
+        if not len(children):
+            if block.get('complete'):
+                progress = 100
+
+            # handle case of empty sections/subsections/units
+            if block.get('type') not in ['course', 'chapter', 'vertical', 'sequential']:
+                unit_count = 1
+
+            block.update({
+                'progress': progress,
+                'unit_count': unit_count,
+            })
+
+        for child in children:
+            self._update_context_with_score_and_progress(child, grade_dict)
+            progress += child.get('progress', 0) * child.get('unit_count', 0)
+            unit_count += child.get('unit_count', 0)
+
+        if children and unit_count:
+            block.update({
+                'progress': round(progress/unit_count, 1),
+                'unit_count': unit_count
+            })
 
     def render_to_fragment(self, request, course_id=None, **kwargs):
         """
-        Render the discussion board to a fragment.
+        Render wikimedia course progress to a fragment.
         Args:
             request: The Django request.
             course_id: The id of the course in question.
 
         Returns:
-            Fragment: The fragment representing the discussion board
+            Fragment: The fragment representing the Wikimedia course progress
         """
         course_key = CourseKey.from_string(course_id)
         user = request.user
 
-        course = get_course_with_access(request.user, 'load', course_key, check_if_enrolled=True)
-        course_grade = CourseGradeFactory().read(user, course)
-        grade_dict = self._get_grade_dict(course_grade)
+        try:
+            course = get_course_with_access(request.user, 'load', course_key, check_if_enrolled=True)
+            course_grade = CourseGradeFactory().read(user, course)
+            grade_dict = self._get_grade_dict(course_grade)
+        except Exception as err:
+            grade_dict = {}
 
         course_outline_context = get_course_outline_block_tree(
             request, course_id, request.user
         )
-        self._update_context_with_score(course_outline_context, grade_dict)
+        self._update_context_with_score_and_progress(course_outline_context, grade_dict)
         html = render_to_string(
             'wikimedia_general/wikimedia_progress_fragment.html',
             {"data": course_outline_context}
         )
-        # inline_js = render_to_string('discussion/test_js.template', context)
 
         fragment = Fragment(html)
         self.add_fragment_resource_urls(fragment)
-        # fragment.add_javascript(inline_js)
         return fragment
-
-
-    # def vendor_js_dependencies(self):
-    #     """
-    #     Returns list of vendor JS files that this view depends on.
-    #     The helper function that it uses to obtain the list of vendor JS files
-    #     works in conjunction with the Django pipeline to ensure that in development mode
-    #     the files are loaded individually, but in production just the single bundle is loaded.
-    #     """
-    #     dependencies = Set()
-    #     dependencies.update(self.get_js_dependencies('discussion_vendor'))
-    #     return list(dependencies)
-
-    # def js_dependencies(self):
-    #     """
-    #     Returns list of JS files that this view depends on.
-    #     The helper function that it uses to obtain the list of JS files
-    #     works in conjunction with the Django pipeline to ensure that in development mode
-    #     the files are loaded individually, but in production just the single bundle is loaded.
-    #     """
-    #     return self.get_js_dependencies('discussion')
-
-    # def css_dependencies(self):
-    #     """
-    #     Returns list of CSS files that this view depends on.
-    #     The helper function that it uses to obtain the list of CSS files
-    #     works in conjunction with the Django pipeline to ensure that in development mode
-    #     the files are loaded individually, but in production just the single bundle is loaded.
-    #     """
-    #     if get_language_bidi():
-    #         return self.get_css_dependencies('style-discussion-main-rtl')
-    #     else:
-    #         return self.get_css_dependencies('style-discussion-main')
