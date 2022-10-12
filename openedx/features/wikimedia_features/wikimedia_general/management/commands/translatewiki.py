3dcc1972bccb6f76f075d60f2c700f31064aeb7b
Wikimedia/edx-platform <wiki-bot@example.com>
2022-10-12 18:38:36 +0500

    add localization flow

---
+++ openedx/features/wikimedia_features/wikimedia_general/management/commands/translatewiki.py
@@ -0,0 +1,424 @@
+"""
+Django admin command to update translations to xblock, mongodb.
+"""
+import os
+import yaml
+import shutil
+from datetime import datetime
+
+from i18n.execute import execute
+
+from django.core.management.base import BaseCommand
+from logging import getLogger
+from polib import pofile, POFile, POEntry
+
+log = getLogger(__name__)
+
+class Command(BaseCommand):
+    """
+    Localization commands
+    """
+    help = 'Commad to automate localization via Trarnslatewiki and Transifex'
+
+    EDX_TRANSLATION_PATH = 'conf/locale'
+
+    def add_arguments(self, parser):
+        """
+        Add --commit argument with default value False
+        """
+        parser.add_argument(
+            'action',
+            choices=[
+                'push',
+                'pull',
+                'msgmerge',
+                'update_translations',
+                'generate_custom_strings',
+                'update_from_translatewiki',
+            ],
+            help='Send translations to Edx Database',
+        )
+        parser.add_argument(
+            '-l', '--languages', nargs='+', help='Specify languages', default=[],
+        )
+        parser.add_argument(
+            '-u', '--username', help='git username', default='edx-platfrom',
+        )
+        parser.add_argument(
+            '-e', '--email', help='git email', default='edx@edx.com',
+        )
+
+    def _validating_files(self, dir, files):
+        """
+        Check if files exists in the directory
+        Arguments:
+            dir: (src) directory path
+            files: (list) files to check in a directory
+        """
+        log.info(f'Validating files in {dir}')
+        for filename in files:
+            file_path = f'{dir}/{filename}'
+            if not os.path.exists(dir):
+                raise ValueError(f"Tranlatewiki: file doesn't exist: {file_path}")
+    
+    def _clone_repo_or_pull_latest_changes(self, token, username, repo_name, to_directory='.'):
+        """
+        Clone or pull latest changes from master branch
+        Arguments:
+            token: (str) Github access token
+            username: (str) Github username
+            repo_name: (str) Github repository name
+            to_directory: (str) Location to clone github repo
+        """
+        repo_location = '{}/{}'.format(to_directory, repo_name)
+        if not os.path.exists(repo_location):
+            log.info(f'cloning {repo_name} to {repo_location}')
+            origin = f'https://{token}@github.com/{username}/{repo_name}.git'
+            execute(f'git clone {origin}', '.')
+        else:
+            log.info(f'Updating {repo_location} master branch')
+            execute(f'git pull origin master', repo_location)
+    
+    def _push_chnages_to_repository(self, repo_name, git_username, git_email, to_directory='.'):
+        """
+        Push the changest to git repository
+        Arguments:
+            repo_name: (str) Github repository name
+            git_username: (str) Username on commit message
+            git_email: (str) Email on commit message
+            to_directory: (str) repository path
+        """
+        repo_location = '{}/{}'.format(to_directory, repo_name)
+        if os.path.exists(repo_location):
+            log.info('Trying commiting latest changes to github')
+            execute(f'git config user.name "{git_username}"', repo_location)
+            execute(f'git config user.email "{git_email}"', repo_location)
+            execute("git add .;git commit -m 'Translations Update';git push origin master", repo_location)
+        else:
+            raise ValueError(f"Translatewiki: Can't push files becuase repo doesn't exist: {repo_location}")
+
+    def _copy_translation_files_from_src_to_dest(self, src_dir, dest_dir, locales, files):
+        """
+        Copy files from source translation directory to destination directory
+        Arguments:
+            src_dir: (str) source root directory i.e 'conf/locale'
+            dest_dir: (str) destination root directory i.e 'edx-platform-translations'
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+        """
+        for lang in locales:
+            src_path = f'{src_dir}/{lang}/LC_MESSAGES'
+            dest_path = f'{dest_dir}/{lang}/LC_MESSAGES'
+            self._validating_files(src_path, files)
+            log.info(f'Copying files from {src_path} to {dest_path}')
+            for file_name in files:
+                os.makedirs(os.path.dirname(f'{dest_path}/{file_name}'), exist_ok=True)
+                shutil.copy(f'{src_path}/{file_name}', f'{dest_path}/{file_name}')
+    
+    def _move_files_from_src_to_dest(self, src_dir, dest_dir, files, delete_src_dir_if_empty=False):
+        """
+        More files from source directory to destination directory
+        Arguments:
+            src_dir: (str) source root directory i.e 'conf/locale/LC_MESSAGES/ar'
+            dest_dir: (str) destination root directory i.e 'conf/locale/LC_MESSAGES/ar/wm'
+            files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+            delete_src_dir_if_empty: (bool) If True, delete source folder if empty
+        """
+        self._validating_files(src_dir, files)
+        if not os.path.exists(dest_dir):
+            log.info(f'Creating a directory : {dest_dir}')
+            os.mkdir(dest_dir)
+        
+        log.info(f'Moving files from {src_dir} to {dest_dir}')
+        for filename in files:
+            soure_file = f'{src_dir}/{filename}'
+            output_file = f'{dest_dir}/{filename}'
+            shutil.move(soure_file, output_file)
+        
+        if delete_src_dir_if_empty and not len(os.listdir(src_dir)):
+            log.info(f'Deleting source directory {src_dir}')
+            os.rmdir(src_dir)
+
+    def pull(self, locales, staged_files, update_edx_files=True):
+        """
+        Clone or Update the repository and copy files from repo to edx directory
+        Arguments:
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            staged_files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+            update_edx_files: (bool) if Fasle it will not update edx translation files
+        """
+        git_access_token = os.getenv('TRANSLATION_REPO_ACCESS_TOKEN')
+        git_organization = os.getenv('TRANSLATION_REPO_ORGANIZATION')
+        git_repo_name = os.getenv('TRANSLATION_REPO_NAME')
+        if not git_access_token:
+            raise ValueError("token not found, please set TRANSLATION_REPO_ACCESS_TOKEN as an env variable")
+        if not git_organization:
+            raise ValueError('organization not found, please set TRANSLATION_REPO_ORGANIZATION as an env variable')
+        if not git_repo_name:
+            raise ValueError('repository-name not found, please set TRANSLATION_REPO_NAME as an env variable')
+
+        self._clone_repo_or_pull_latest_changes(
+            git_access_token, git_organization, git_repo_name,
+        )
+        if update_edx_files:
+            repo_files = set(os.listdir(git_repo_name))
+            locales = list(set(locales).intersection(repo_files))
+            self._copy_translation_files_from_src_to_dest(
+                git_repo_name, self.EDX_TRANSLATION_PATH, locales, staged_files,
+            )
+    
+    def push(self, locales, staged_files, git_username, git_email, updete_remote_repo=True):
+        """
+        Copy files from edx to repository and Push changes to master branch of 
+        the translation repository
+        Arguments:
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            staged_files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+            git_username: (str) git username on commit message
+            git_email: (str) git email on commit message
+            updete_remote_repo: (bool) if Fasle it will not update remote repository
+        """
+        git_repo_name = os.getenv('TRANSLATION_REPO_NAME')
+        if not git_repo_name:
+            raise ValueError('repository-name not found, please set TRANSLATION_REPO_NAME as an env variable')
+
+        self._copy_translation_files_from_src_to_dest(
+            self.EDX_TRANSLATION_PATH, git_repo_name, locales, staged_files
+        )
+        if updete_remote_repo:
+            self._push_chnages_to_repository(git_repo_name, git_username, git_email)
+
+    def msgmerge(self, locales, staged_files, base_lang='en', generate_po_file_if_not_exist=False, output_file_mapping={}):
+        """
+        Merge base language transaltions with other languages
+        Arguments:
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            staged_files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+            base_lang: (str) base language of edx-platform
+            generate_po_file_if_not_exist: (bool) if True and destination path doesn't exist, it will create empty po file
+            output_file_mapping: (dict) used to generate metadata when creatring new .po file
+        """
+        msgmerge_command = 'msgmerge {to} {source} --update --no-fuzzy-matching'
+        locales = list(set(locales) - set([base_lang]))
+        edx_translation_path = self.EDX_TRANSLATION_PATH
+        from_path = f'{edx_translation_path}/{base_lang}/LC_MESSAGES'
+        for lang in locales:
+            to_path = f'{edx_translation_path}/{lang}/LC_MESSAGES'
+            log.info(f'Merging {base_lang} translations with {lang}')
+            for filename in staged_files:
+                from_file = f'{from_path}/{filename}'
+                to_file = f'{to_path}/{filename}'
+                if generate_po_file_if_not_exist and not os.path.exists(to_file):
+                    log.info(f'{to_file} not exist, Creating {filename} in {to_path}')
+                    meta_data = self._get_metadata_from_po_file(f'{to_path}/{output_file_mapping[filename]}')
+                    self._create_or_update_po_file(to_file, [], meta_data)
+                command = msgmerge_command.format(to=to_file, source=from_file)
+                execute(command)
+    
+    def update_translations_from_transifex(self, locales, staged_files, base_lang='en'):
+        """
+        Merge base language transaltions with other languages
+        Arguments:
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            staged_files: (list) files to copy i.e ['wiki.po', 'wikijs.po']
+            base_lang: (str) base language of edx-platform
+        """
+        transifex_token = os.getenv('TX_TOKEN')
+        if not transifex_token:
+            raise ValueError(
+                'Translatewiki: Transifex token not found, set TX_TOKEN as an env variable'
+            )
+        
+        pomerge_command = 'pomerge --from {from_path} --to {to_path}'
+        locales = list(set(locales) - set([base_lang]))
+
+        edx_translation_path = self.EDX_TRANSLATION_PATH
+        for lang in locales:
+            edx_dir = f'{edx_translation_path}/{lang}/LC_MESSAGES'
+            wm_dir = f'{edx_translation_path}/{lang}/LC_MESSAGES/wm'
+
+            self._move_files_from_src_to_dest(edx_dir, wm_dir, staged_files)
+            
+            log.info(f'Pulling {lang} transaltions from Transifex')
+            execute(f'tx pull --mode=reviewed -l {lang}')
+            
+            log.info(f'Merging Transifex {lang} files to platform {lang} files')
+            for filename in staged_files:
+                command = pomerge_command.format(
+                    from_path = f'{edx_dir}/{filename}',
+                    to_path = f'{wm_dir}/{filename}'
+                )
+                execute(command)
+            
+            self._move_files_from_src_to_dest(
+                wm_dir, edx_dir, staged_files, delete_src_dir_if_empty=True
+            )
+        
+        log.info(f'{locales} are updated with Transifex Translations')
+
+    def update_translations_from_translatewiki(self, locals, merge_scheme):
+        """
+        Merge translations of Translatewiki with Transifex
+        """
+        pomerge_command = 'pomerge --from {from_path} --to {to_path}'
+        edx_translation_path = self.EDX_TRANSLATION_PATH
+
+        for lang in locals:
+            edx_dir = f'{edx_translation_path}/{lang}/LC_MESSAGES'
+            for source_file, files in merge_scheme.items():
+                for filename in files:
+                    log.info(f'Updating {edx_dir}/{filename} from {edx_dir}/{source_file}')
+                    command = pomerge_command.format(
+                        from_path=f'{edx_dir}/{source_file}',
+                        to_path=f'{edx_dir}/{filename}',
+                    )
+                    execute(command)
+    
+    def _get_metadata_from_po_file(self, path):
+        """
+        Returns metadata of po file
+        Argument:
+            path: (str) po file path i.e conf/locale/LC_MESSAGES/en/django.po
+        """
+        try:
+            pomsgs = pofile(path)
+            return pomsgs.metadata
+        except:
+            return {}
+
+    def _get_msgids_from_po_file(self, path):
+        """
+        Extract pomsgs and unique ids from po file
+        Argument:
+            path: (str) po file path i.e conf/locale/LC_MESSAGES/en/django.po
+        """
+        pomsgs = pofile(path)
+        poids = set([entry.msgid for entry in pomsgs])
+        return pomsgs, poids
+
+    def _create_or_update_po_file(self, output_file, po_entries, po_meta_data, add_fuzzy=False):
+        """
+        Create or update po file from list of PoEntry
+        Arguments:
+            output_file: (str) output file path i.e 'conf/locale/LC_MESSAGES/en/wm-djangojs.po'
+            po_entries: (list) list of POEntry
+            po_metadata: (dict) metadata used while creating po file
+            add_fuzzy: (bool) If True, add ' ,fuzzy' in header
+        """
+        if os.path.exists(output_file):
+            if po_entries:
+                pomsgs = pofile(output_file)
+                for entry in po_entries:
+                    if not pomsgs.find(entry.msgid):
+                        pomsgs.append(entry)
+                if pomsgs.metadata.get('PO-Revision-Date'):
+                    pomsgs.metadata['PO-Revision-Date'] = str(datetime.now())
+                pomsgs.save(output_file)
+        else:
+            po = POFile()
+            date = datetime.now()
+            po_meta_files = {
+                'POT-Creation-Date': str(date),
+                'PO-Revision-Date': str(date),
+                'Report-Msgid-Bugs-To': 'Translatewiki',
+                'Last-Translator': '',
+                'Language-Team': 'Translatewiki',
+            }
+            po_meta_data.update(po_meta_files)
+            po.metadata = po_meta_data
+            if add_fuzzy:
+                po.header = ', fuzzy'
+            for entry in po_entries:
+                po.append(entry)
+            po.save(output_file)
+
+    def generate_custom_strings(self, target_files, locales, base_lang='en', prefix='wm'):
+        """
+        Merge base language transaltions with other languages
+        Arguments:
+            target_files: (list) target files i.e ['django.po', 'djangojs.po']
+            locales: (list) list of languages i.e ['ar', 'en', 'fr']
+            base_lang: (str) base language of edx-platform
+            prefix: (str) prefix on new generated files
+        """
+        transifex_token = os.getenv('TX_TOKEN')
+        if not transifex_token:
+            raise ValueError('Translatewiki: Transifex token not found, set TX_TOKEN as an env variable')
+
+        edx_translation_path = self.EDX_TRANSLATION_PATH
+        locales = list(set(locales) - set([base_lang]))
+
+        edx_dir = f'{edx_translation_path}/{base_lang}/LC_MESSAGES'
+        wm_dir = f'{edx_translation_path}/{base_lang}/LC_MESSAGES/wm'
+
+        self._move_files_from_src_to_dest(edx_dir, wm_dir, target_files)
+        
+        log.info(f'Pulling {base_lang} transaltions from Transifex')
+        execute(f'tx pull --mode=reviewed -l {base_lang}')
+        output_files = []
+        for filename in target_files:
+            log.info(f'Generating new po file from {edx_dir}/{filename}')
+            _, tx_ids = self._get_msgids_from_po_file(f'{edx_dir}/{filename}')
+            edx_msgs, edx_ids = self._get_msgids_from_po_file(f'{wm_dir}/{filename}')
+            custom_ids = edx_ids - tx_ids
+            po_entries = [edx_msgs.find(msgid) for msgid in custom_ids]
+            output_file = f'{edx_dir}/{prefix}-{filename}'
+            self._create_or_update_po_file(
+                output_file, po_entries, edx_msgs.metadata, add_fuzzy=True,
+            )
+            output_files.append(f'{prefix}-{filename}')
+        
+        self._move_files_from_src_to_dest(wm_dir, edx_dir, target_files, delete_src_dir_if_empty=True)
+        files_mapping = dict(zip(output_files, target_files))
+        self.msgmerge(locales, output_files, generate_po_file_if_not_exist=True, output_file_mapping=files_mapping)
+        
+        log.info(f'{len(output_files)} new file(s) are created {output_files}')
+    
+
+    def process_configuration_file(self, filepath):
+        """
+        Process configuration file to get locals and untracked files
+        Argument:
+            filepath: localization config file i.e conf/locale/config.yaml
+        """
+        configuration = {}
+        with open(filepath, "r") as stream:
+            configuration = yaml.safe_load(stream)
+        locales = configuration.get('locales')
+        merge_scheme = configuration.get('generate_merge')
+        staged_files = []
+        targated_files = []
+        for key, value in merge_scheme.items():
+            targated_files.append(key)
+            staged_files.extend(value)
+
+        return locales, targated_files, staged_files, merge_scheme
+
+    def handle(self, *args, **options):
+        """
+        Handle Translatewiki Localization Opeerations
+        """
+        locales, targated_files, staged_files, merge_scheme = self.process_configuration_file('conf/locale/config.yaml')
+        languages = options.get('languages', [])
+        wm_files = [f'wm-{filename}' for filename in targated_files]
+        
+        if languages:
+            if len(set(locales) - set(languages)) == len(set(locales)):
+                raise ValueError(f'Invaild Languages, valid languages are {locales}')
+            locales = languages
+        
+        if options['action'] == 'pull':
+            self.pull(locales, wm_files)
+        elif options['action'] == 'push':
+            username = options.get('username')
+            email = options.get('email')
+            self.push(locales, wm_files, username, email)
+        elif options['action'] == 'msgmerge':
+            self.msgmerge(locales, staged_files)
+        elif options['action'] == 'update_translations':
+            self.update_translations_from_transifex(locales, staged_files)
+        elif options['action'] == 'generate_custom_strings':
+            self.generate_custom_strings(targated_files, locales)
+        elif options['action'] == 'update_from_translatewiki':
+            scheme = {f'wm-{key}': val for key, val in merge_scheme.items()}
+            self.update_translations_from_translatewiki(locales, scheme)
