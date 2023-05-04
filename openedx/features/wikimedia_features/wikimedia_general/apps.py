82ec9136177a7703897cded9976190b6137e56b6
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2023-05-04 16:00:21 +0500

    feat: add font API

--- openedx/features/wikimedia_features/wikimedia_general/apps.py
+++ openedx/features/wikimedia_features/wikimedia_general/apps.py
@@ -10,6 +10,18 @@ class WikimediaGeneralConfig(AppConfig):
     name = 'openedx.features.wikimedia_features.wikimedia_general'
 
     plugin_app = {
+        PluginURLs.CONFIG: {
+            ProjectType.CMS: {
+                    PluginURLs.NAMESPACE: 'wikimedia_general',
+                    PluginURLs.REGEX: '^wikimedia_general/',
+                    PluginURLs.RELATIVE_PATH: 'urls',
+                },
+            ProjectType.LMS: {
+                    PluginURLs.NAMESPACE: 'wikimedia_general',
+                    PluginURLs.REGEX: '^wikimedia_general/',
+                    PluginURLs.RELATIVE_PATH: 'urls',
+                },
+        },
         PluginSettings.CONFIG: {
             ProjectType.LMS: {
                 SettingsType.COMMON: {PluginSettings.RELATIVE_PATH: 'settings.common'},
--- openedx/features/wikimedia_features/wikimedia_general/urls.py
+++ openedx/features/wikimedia_features/wikimedia_general/urls.py
@@ -1,15 +1,20 @@
 """
 Forum urls for the django_comment_client.
 """
-from django.conf.urls import url, patterns
+from django.conf.urls import url, include
 
 from .views import WikimediaProgressFragmentView
 
-urlpatterns = patterns(
-    'wikimedia_general.views',
+app_name = 'wikimedia_general'
+
+urlpatterns = [
     url(
         r'progress_fragment_view$',
         WikimediaProgressFragmentView.as_view(),
         name='progress_fragment_view'
     ),
-)
+    url(
+        r'^api/v0/',
+        include('openedx.features.wikimedia_features.wikimedia_general.api.v0.urls', namespace='general_api_v0')
+    ),
+]
