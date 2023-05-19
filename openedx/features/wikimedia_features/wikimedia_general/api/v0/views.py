4abfecc40652ee71a5134fec33cbbdc908edc744
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2023-05-19 14:47:39 +0500

    feat: add course recomendation api

--- openedx/features/wikimedia_features/wikimedia_general/api/v0/views.py
+++ openedx/features/wikimedia_features/wikimedia_general/api/v0/views.py
@@ -1,12 +1,22 @@
 """
 Views for wikimedia_general v0 API(s)
 """
+import operator
+from functools import reduce
+
+from django.db.models import Q
+
 from rest_framework import generics, status, permissions
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
 from opaque_keys.edx.keys import CourseKey
 
 from lms.djangoapps.courseware.courses import get_course_by_id
-
+from lms.djangoapps.courseware.courses import get_courses
+from openedx.features.wikimedia_features.wikimedia_general.utils import get_user_completed_course_keys
+from openedx.core.djangoapps.content.course_overviews.serializers import CourseOverviewBaseSerializer
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
 
 class RetrieveWikiMetaData(generics.RetrieveAPIView):
     """
@@ -31,3 +41,19 @@ class RetrieveWikiMetaData(generics.RetrieveAPIView):
         }
 
         return Response(data, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def get_courses_to_study_next(request):
+    """Endpoint to retrieve follow up courses for the user's completed courses.
+    """
+    user = request.user
+
+    user_completed_course_keys = get_user_completed_course_keys(user)
+    # Find courses which have courses in user_completed_course_keys as their prerequisite
+    query = reduce(operator.and_, (Q(_pre_requisite_courses_json__contains=course_key) 
+                                   for course_key in user_completed_course_keys)) 
+    follow_up_courses = list(CourseOverview.objects.filter(query))
+    serialzer = CourseOverviewBaseSerializer(follow_up_courses, many=True)
+        
+    return Response({"follow-up-courses": serialzer.data}, status=status.HTTP_200_OK)
