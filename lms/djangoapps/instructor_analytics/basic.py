a10a8c4dd77089db6987a46bc382860ecf03d833
Abdul-Muqadim-Arbisoft <abdul.muqadim@arbisoft.com>
2024-06-28 10:57:33 +0500

    corrected certifcate links in instructor certificates report

--- lms/djangoapps/instructor_analytics/basic.py
+++ lms/djangoapps/instructor_analytics/basic.py
@@ -52,7 +52,7 @@ SALE_ORDER_FEATURES = ('id', 'company_name', 'company_contact_name', 'company_co
 AVAILABLE_FEATURES = STUDENT_FEATURES + PROFILE_FEATURES + PROGRAM_ENROLLMENT_FEATURES
 COURSE_REGISTRATION_FEATURES = ('code', 'course_id', 'created_by', 'created_at', 'is_valid')
 COUPON_FEATURES = ('code', 'course_id', 'percentage_discount', 'description', 'expiration_date', 'is_active')
-CERTIFICATE_FEATURES = ('course_id', 'mode', 'status', 'grade', 'created_date', 'is_active', 'error_reason')
+CERTIFICATE_FEATURES = ('course_id', 'mode', 'status', 'grade', 'created_date', 'is_active', 'error_reason', 'download_url')
 
 UNAVAILABLE = "[unavailable]"
 
